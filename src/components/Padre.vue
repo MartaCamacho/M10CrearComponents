da3674f789d5ea5224ce207a622adcc026c72a3d
martacamacho <martacamachovargas@gmail.com>
2021-02-16 21:58:02 +0100

    done

---
+++ src/components/Padre.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Padre">
+   <p>{{menssage}}</p>
+    <Hijo></Hijo>
+  </div>
+</template>
+
+<script>
+import Hijo from './Hijo.vue';
+
+export default {
+    components: {
+    Hijo
+  },
+  data() {
+      return {
+          menssage: 'Soc el Pare'
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
